23bb72c359328030c4906c67f3ecc638c99cc31b
sispeo <42068883+fperot74@users.noreply.github.com>
2019-09-23 11:30:57 +0200

    Add constructor to CloudtrustException

--- cloudtrust-common/src/main/java/io/cloudtrust/exception/CloudtrustException.java
+++ cloudtrust-common/src/main/java/io/cloudtrust/exception/CloudtrustException.java
@@ -1,7 +1,10 @@
 package io.cloudtrust.exception;
 
-public class CloudtrustException extends RuntimeException {
+public class CloudtrustException extends Exception {
     private static final long serialVersionUID = 1337533331050583350L;
+
+    public CloudtrustException() {
+    }
 
     public CloudtrustException(String message) {
         super(message);
--- cloudtrust-common/src/main/java/io/cloudtrust/exception/CloudtrustRuntimeException.java
+++ cloudtrust-common/src/main/java/io/cloudtrust/exception/CloudtrustRuntimeException.java
@@ -3,6 +3,9 @@ package io.cloudtrust.exception;
 public class CloudtrustRuntimeException extends RuntimeException {
 	private static final long serialVersionUID = 8438754018850435511L;
 
+	public CloudtrustRuntimeException() {
+	}
+
 	public CloudtrustRuntimeException(String message) {
 		super(message);
 	}
